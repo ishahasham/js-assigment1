4d2e9c479e0aadc673e9d4a5b66f992155de1d27
Isha Hasham <144732430+ishahasham@users.noreply.github.com>
2024-02-22 19:28:58 +0500

    JavaScript assignment

---
+++ app.js
@@ -0,0 +1,157 @@
+// Assignment 14
+
+// CHAP 1-3.DOCX
+
+// chapter  1 (alert)
+
+// 1-Alert following 
+// alert ("Isha");
+// alert ("Hasham");
+// alert ("ishahasham@gmail.com");
+// alert("0336-8935495");
+// alert("password");
+
+// 2-Correct statement 
+// alert("You're learning JavaScript!");
+
+// 3-Code an alert statement displaying any you like.
+// alert("ASSIGNMENT 14");
+
+// Chapter 2 (Variable for string)
+
+// 1-Declare any variable in the camel case format 
+// var firstName="Isha";
+// console.log(firstName);
+
+// 2-declare a variable of your choice without defining it. then, in a second statement statement, assign it a string of your choice.
+// var myCode="Hello World!";
+// var myVariable=(myCode);
+// console.log(myVariable);
+
+// 3-Declare the variable teamName and Alert your Team name.
+// var teamName="Peshawar Zalmi";
+// alert(teamName);
+
+// 4-This statement has already been coded. var bestMan = "Charlie" Assign the variable a new string.
+// var bestMan="Charlie";
+// var bestCricketer=(bestMan);
+// console.log(bestCricketer);
+
+// Chap-3 (Variables for numbers):
+
+// these value are all are used in one code from declaring variable to math operation
+// 1- Declare a variable “caseQty”
+// var caseQty;
+
+// 2-Assign to the variable caseQty, which has already been declared, the value 144.
+// var num=(144);
+
+//3-Rewrite this statement so the variable can be used in a math operation.
+// var dividing=(num/16);
+// console.log(dividing);
+
+// 4-In one statement declare a variable. In a second statement assign it the sum of 2 numbers.
+// var sum;
+// sum=4+2;
+// console.log(sum);
+
+// 5-What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge; 
+// console.log(orderTotal);
+
+// 6-In the first statement declare a variable and assign it a number. In
+//  the second statement, change the value of the variable by adding
+// it together with a number.
+// var num1= 10;
+// num1= num1 + 12;
+// console.log(num1);
+
+
+// CHAPTER ALERT: 
+// question 1
+// alert("Welcome Everyone!");
+
+// question 2
+// alert("Error! Please enter a valid password.");
+
+// question 3
+// alert("Welcome to JS Land.. \n Happy Coding!");
+
+// question 4 
+// alert("Welcome to JS Land..");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+// question 5 
+// var code=("Hello.. I can run JS through my web browser's console");
+// alert(code);
+
+// CHAPTER STRING:
+// question 1
+// var username;
+
+// question 2
+// var myName="Syeda Isha Hasham";
+// console.log(myName);
+
+// question 3
+// var JS;
+// JS="Hello World";
+// alert(JS);
+
+// question 4
+// var name="Jhone Doe";
+// var age="15 years old";
+// var mobile="Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(mobile);
+
+// question 5
+// var JS=" PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(JS);
+
+// question 6
+// var email="ishahasham@gmail.com";
+// var statement="My email address is ";
+// var final=(statement+email);
+// alert(final);
+
+// question 7
+// var book="A smarter way to learn JavaScript";
+// var statement="I am trying to learn from the book, "
+// var final=(statement+\n+book);
+// alert(final);
+
+// question 8
+// var JS="Yah! I can write HTML content through JavaScript"
+// document.write(JS);
+
+// question 9
+// var JS="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(JS);
+
+// CHAPTER VARIABLES FOR NUMBER:
+
+// question 1
+// var age=15;
+// var statement1="I am ";
+// var statement2=" years old";
+// alert (statement1+age+statement2);
+
+// question 2
+// var visit=14;
+// var statement1="You have visited this site ";
+// var statement2=" times";
+// alert (statement1+age+statement2);
+
+// question 3
+// var birthYear=2003;
+// var type=(typeof birthYear)
+// document.write("My birth year is " +birthYear + " <br> Data type of my declared variable is " + type);
+
+// question 4
+// var name="<b>John Doe</b>";
+// var order="<b>5 T-shirt</b>(s)";
+// document.write(name + " ordered " + order +" on XYZ Clothing store.")
